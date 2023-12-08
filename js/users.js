c2ed47c03d0b4be7f36b9b281451a1761d96e8f9
RichiLM <richar12.rlom@gmail.com>
2023-12-07 23:36:44 -0600

    Añadi api y usuarios

---
+++ js/users.js
@@ -0,0 +1,28 @@
+const apiURL = 'https://reqres.in/api/users?page=2';
+
+const XML = new XMLHttpRequest();
+
+function onRequestHandler() {
+    if (this.readyState == 4 && this.status == 200) {
+        const response = JSON.parse(this.responseText);
+        const data = response.data; // Accede al array de usuarios
+
+        const HTMLResponse = document.querySelector('.usuario');
+
+        const template = data.map((user) => 
+        `<div class="usuario__imagen">
+            <img class="img-avatar" src="${user.avatar}" alt ="Avatar usuario">
+        </div>
+        <div class="usuario__descripcion">
+            <p><span class="bold-text">Nombre:</span> ${user.first_name}</p>
+            <p><span class="bold-text">Apellido:</span> ${user.last_name}</p>
+            <p><span class="bold-text">Correo:</span> ${user.email}</p>
+        </div>
+        `);
+        HTMLResponse.innerHTML = `${template.join('')}`; // Utiliza join para convertir el array en una cadena
+    }
+}
+
+XML.addEventListener('load', onRequestHandler);
+XML.open('GET', apiURL);
+XML.send();
